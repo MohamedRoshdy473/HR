181f901174bca8ec7cd80164fc2367d9d35cca82
MohamedRoshdy473 <60385152+MohamedRoshdy473@users.noreply.github.com>
2021-04-08 14:35:29 +0200

    push service

---
+++ HR.Service/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HR.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Service
+{
+    public interface IUnitOfWork
+    {
+        int CommitAsync();
+        IEmployeeRepository employee { get; }
+    }
+}
---
+++ HR.Service/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using HR.Models;
+using HR.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Service
+{
+   public class UnitOfWork:IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private  IEmployeeRepository _employeeRepository;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEmployeeRepository employeeRepository => new EmployeeRepositories(_context);
+
+        public int CommitAsync()
+        {
+            return _context.SaveChanges();
+        }
+         public IEmployeeRepository employee => _employeeRepository = _employeeRepository ?? new EmployeeRepositories(_context);
+
+    }
+}
+
