c2adcf0d3389e167bce032cbf8cf99e1ae9fe614
MohamedRoshdy473 <60385152+MohamedRoshdy473@users.noreply.github.com>
2021-04-08 14:34:30 +0200

    push repo

---
+++ HR.DataAccess.Repository/Repositories/EmployeeRepositories/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using HR.DTO;
+using HR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR.Repositories
+{
+    public interface IEmployeeRepository
+        //:IRepository<Employee>
+    {
+        IEnumerable<EmployeeDTO> EmployeeByProfession(int EmpId);
+        IEnumerable<EmployeeDTO> GetAllEmployeesDTO();
+        EmployeeDTO GetEmployee(int id);
+    }
+}
---
+++ HR.DataAccess.Repository/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        Task Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
